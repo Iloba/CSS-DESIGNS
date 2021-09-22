bb93341cb1c99f8d60f26edb695559e460975760
Iloba <scarlettsney@gmail.com>
2021-09-22 13:10:08 +0100

    swiper-js

--- js/script.js
+++ js/script.js
@@ -17,3 +17,19 @@ document.querySelector('#search-icon').onclick = () =>{
 document.querySelector('#close').onclick = () =>{
 	document.querySelector('#search-form').classList.remove('active');
 }
+
+// Swiper JS
+
+var swiper = new Swiper(".home-slider", {
+	spaceBetween: 30,
+	centeredSlides: true,
+	autoplay: {
+	  delay: 7500,
+	  disableOnInteraction: false,
+	},
+	pagination: {
+	  el: ".swiper-pagination",
+	  clickable: true,
+	},
+	loop: true,
+});
